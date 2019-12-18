c7a0b9f044e48d6785b90a43d604a583ed7fe6c6
lilindog <lilin@lilin.site>
2019-12-18 15:44:17 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const fs = require("fs");
+
+/**
+ * 解析自定义.env配置文件为json
+ * 
+ * @param {String} path 后缀名为.env的配置文件路径
+ * @return {Object}
+ */
+module.exports = function (envPath) {
+    if (!fs.existsSync(envPath)) {
+        throw `${envPath} 不存在！`;
+    }
+    let 
+    data = {},
+    str = fs.readFileSync("./test.env").toString();
+    str = str.replace(/\r{2}/g, "\r");
+    str = str.replace(/\n/g, "");
+    str = str.split("\r");
+    str = str.filter(item => !~item.indexOf("#") && item !== "" && ~item.indexOf("="));
+    str = str.map(item => item.replace(/\s/g, ""));
+    // console.log(str);
+    str = str.map(item => {
+        data[item.split("=")[0]] = item.split("=")[1];
+    });
+    // console.log(data);
+    return data;
+}
---
+++ test/test.js
@@ -0,0 +1,2 @@
+const parseEnv = require("../index");
+console.log(parseEnv("./test.env"));
