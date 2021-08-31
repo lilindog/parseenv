ef1d4708aa800dd30a9e301cdd6aeb0f4da2bdda
lilindog <lilin@lilin.site>
2021-08-31 14:41:28 +0800

    补充ts类型声明文件注释

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 declare module "parseenv" {
-    export default function (str: String): parseenvReslut;
+
+    // envPath 为.env文件的路径
+    // 可以是绝对路径，也可以是基于当继承cwd的相对路径
+    export default function (envPath: String): parseenvReslut;
+
     export interface parseenvReslut {
         [key: String]: String|Number|Array<String|Number>
     }
