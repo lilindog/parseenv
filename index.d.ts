1dc392dec6b1eda465265f0e8797be555f03b649
lilin <lilin@lilin.site>
2021-08-31 14:52:18 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,9 @@ declare module "parseenv" {
 
     // envPath 为.env文件的路径
     // 可以是绝对路径，也可以是基于当前进程cwd的相对路径
-    export default function (envPath: String): parseenvReslut;
+    export default function (envPath: String): parseenvResult;
 
-    export interface parseenvReslut {
+    export interface parseenvResult {
         [key: String]: String|Number|Array<String|Number>
     }
 }
