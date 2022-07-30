75a4d3e9740cbc22913a1f50499985e3a9202f06
lilindog <lilin@lilin.site>
2022-07-31 00:18:53 +0800

    规整githooks相关脚本

--- scripts/init-githooks.js
+++ scripts/init-githooks.js
@@ -1,61 +1,56 @@
-"use strict"
-
-/**
- * 初始化githook 
- * 
- * 需要脚本目录有相应的文件才可以被正确部署。
- * 脚本问文件名字需要 githook_ 作为前缀，后面的明明参考git文档给出的hook文件名，需要有js扩展名。
- */
+"use strict";
 
 const { writeFileSync, existsSync, readdirSync } =require("fs");
-const { parse } = require("path");
+const { parse, resolve, sep } = require("path");
 const platform = require("os").platform();
 const { execSync } = require("child_process");
 
-// 存放脚本的目录
+const log = console.log;
+
+const MESSAGE_MAP = {
+    START_INSTALL_GITHOOKS: () => log("开始部署本地githooks ..."),
+    INSTALLED_NONE: () => log("githooks 部署完成，没有githook被部署！"),
+    INSTALLED_SUCCESS: count => log(`githooks 部署完成，共有${count}个hook被部署`)
+};
+
 const 
-    SCRIPTS_DIR = "scripts",
-    IS_THIS_PROJECT = (process.env.INIT_CWD || "").endsWith(process.env.npm_package_name);
+    SCRIPTS_DIR = resolve(__dirname, '../githooks'),
+    GIT_HOOKS_DIR = resolve(__dirname, "../.git/hooks"),
+    IS_THIS_PROJECT = process.env.npm_config_in_parseenv_project === "YES";
 
 IS_THIS_PROJECT && main();
 
 function main () {
-    console.log("开始部署本地githooks >>");
-    const writedScripts = [], reg = /pre-\w+(?=\.js)/;
-    readdirSync("scripts").filter(i => reg.test(i)).map(i => i.match(reg)).forEach(name => {
-        const path = `${SCRIPTS_DIR}/${name}.js`;
-        if (existsSync(path)) {
-            writedScripts.push(path);
-            writeHookFile(path);
-        }
+    MESSAGE_MAP.START_INSTALL_GITHOOKS();
+    const writedScripts = [];
+    const reg = /pre-\w+(?=\.js)/;
+    readdirSync(SCRIPTS_DIR)
+    .filter(i => reg.test(i))
+    .map(i => i.match(reg))
+    .forEach(name => {
+        let script = resolve(SCRIPTS_DIR, `${name}.js`);
+        if (sep === "\\") script = script.replaceAll(sep, "\\\\");
+        if (!existsSync(script)) return;
+        writedScripts.push(script);
+        generateHookFile(script, name);
     });
-    if (writedScripts.length) {
-        platform !== "win32" && execSync(`chmod -R 777 .git/hooks`);
-        console.log(`githooks 部署完成，共有${writedScripts.length}个hook被部署：`);
-        writedScripts.forEach((path, index) => {
-            console.log(`(${index + 1}) -> ${path}`);
-        });
-    } else {
-        console.log("githooks 部署完成，没有githook被部署！");
-    }
-    console.log("");
+    if (!writedScripts.length) return MESSAGE_MAP.INSTALLED_NONE();
+    platform !== "win32" && execSync(`chmod -R 777 ${GIT_HOOKS_DIR}`);
+    MESSAGE_MAP.INSTALLED_SUCCESS(writedScripts.length);
+    writedScripts.forEach((path, index) => log(`(${index + 1}) -> ${parse(path).name}`));
 }
 
-function writeHookFile (path) {
+function generateHookFile (script, name) {
+    const reg = /[\n\s]+/ig
     writeFileSync(
-        `.git/hooks/${parse(path).name}`, 
-        `#!/usr/bin/env node
+        `.git/hooks/${name}`,
+        `#!/usr/bin/env node\n` +
+        `
         const { spawn } = require("child_process");
-        const cp = spawn("node", ["${path}"], { stdio: "inherit" });
+        const cp = spawn("node", ["${script}"], { stdio: "inherit" });
         cp.on("exit", code => {
             process.exit(code);
         });
-        `
-        .replace(/\s/, "<sign1>")
-        .replace(/\n/, "<sign2>")
-        .replace(/\s+/g, " ")
-        .replace(/\n/g, "")
-        .replace("<sign1>", " ")
-        .replace("<sign2>", "\n")
+        `.replace(/[\n\s]+/g, ' ')
     );
 }
